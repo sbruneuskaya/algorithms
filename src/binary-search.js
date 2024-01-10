3cf8309f5cd0733920db6fff8b6e341cef3fc7a8
Bruneuskaya <bs@primexbt.ch>
2024-01-10 13:37:33 +0300

    main: added recursive binary search and quick sort

--- src/binary-search.js
+++ src/binary-search.js
@@ -1,4 +1,4 @@
-const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+const arrNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 let countArr = 0
 
 function binarySearch(array, item) {
@@ -30,5 +30,22 @@ function binarySearch(array, item) {
     return position
 }
 
-// console.log('BINARY', binarySearch(arr, 7))
+// console.log('BINARY', binarySearch(arrNumbers, 7))
+// console.log(countArr)
+
+function recursiveBinarySearch(array, item, start, end) {
+    let middle = Math.floor((start + end) / 2)
+    countArr +=1
+    if (item === array[middle]) {
+        return middle
+    }
+
+    if (item < array[middle]) {
+        return recursiveBinarySearch(array, item, start, middle - 1)
+    } else {
+        return recursiveBinarySearch(array, item, middle + 1, end)
+    }
+}
+
+// console.log(recursiveBinarySearch(arrNumbers, 7, 0, arrNumbers.length))
 // console.log(countArr)
--- src/quick-sort.js
+++ src/quick-sort.js
@@ -1,6 +1,35 @@
-const arrQuickSort = [0,3,2,5,6,8,23,9,4,2,1,2,9,6,4,1,7,-1, -5, 23,6,2,35,6,3,32,9,4,2,1,2,9,6,4,1,7,-1, -5, 23,9,4,2,1,2,9,6,4,1,7,-1, -5, 23,]
+const arrQuickSort = [
+    0, 3, 2, 5, 6, 8, 23, 9, 4, 2, 1, 2, 9, 6, 4, 1, 7,
+    -1, -5, 23, 6, 2, 35, 6, 3, 32, 9, 4, 2, 1, 2, 9, 6,
+    4, 1, 7, -1, -5, 23, 9, 4, 2, 1, 2, 9, 6, 4, 1, 7,
+    -1, -5, 23
+]
 let countQuickSort = 0
 
-function quickSort(){
+function quickSort(array) {
+    if (array.length <= 1) {
+        return array
+    }
 
+    let centerIndex = Math.floor(array.length / 2)
+    let centerElem = array[centerIndex]
+    let lessNumberArr = []
+    let bigNumberArr = []
+
+    for (let i = 0; i < array.length; i++) {
+        countQuickSort +=1
+        if (i === centerIndex) {
+            continue
+        }
+        if (array[i] < centerElem) {
+            lessNumberArr.push(array[i])
+        } else {
+            bigNumberArr.push(array[i])
+        }
+    }
+
+    return [...quickSort(lessNumberArr), centerElem, ...quickSort(bigNumberArr)]
 }
+
+// console.log(quickSort(arrQuickSort))
+// console.log(countQuickSort)
